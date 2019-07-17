be4a96ff1f8d4ceddcfe9455e1d0a14a1e8704d3
darpanhbatta <bhatta.darpan@gmail.com>
2019-07-16 19:31:35 -0500

    dog class

--- src/Bucky.java
+++ src/Bucky.java
@@ -4,11 +4,20 @@ public class Bucky {
     public static void main(String[] args) {
         Scanner bucky = new Scanner(System.in);
 
+        /*
         Tuna tunaObject = new Tuna();
         System.out.println("Enter your name");
         String name = bucky.nextLine();
 
         tunaObject.simpleMessage(name);
+
+        */
+
+        Dog jeff = new Dog(false, "Jeff",0,5.5);
+
+        System.out.println(jeff.isSpotted());
+        jeff.setSpotted(false);
+        System.out.println(jeff.isSpotted());
 
 
     }
